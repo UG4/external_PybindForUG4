0c8572fe36097f250c5afaa07d7c1b082cbeca3c
Arne Naegel <naegel@gcsc.uni-frankfurt.de>
2022-12-13 08:44:34 +0100

    Introducing plugins

--- src/ug4demo.py
+++ src/ug4demo.py
@@ -4,15 +4,22 @@ env_ug4_root = os.environ['UG4_ROOT']
 
 import sys
 sys.path.append(env_ug4_root+'/lib/')
+sys.path.append(env_ug4_root+'/bin/plugins/ug4py')
 print(sys.path)
 
 import ug4py as ug4
+import pylimex as limex
+import pyconvectiondiffusion as cd
 
+
+# Initialize UG4
 ug4.InitUG(2, ug4.AlgebraType("CPU", 1))
 
+# Load domain.
 dom = ug4.Domain2d()
-ug4.LoadDomain(dom, env_ug4_root + "/apps/Examples/heatsink2d/heatsink.ugx")
+ug4.LoadDomain(dom, env_ug4_root + "/apps/Examples/grids/laplace_sample_grid_2d.ugx")
 
+# Create approximation space.
 approxSpace = ug4.ApproximationSpace2d(dom)
 approxSpace.add_fct("u","Lagrange", 1)
 approxSpace.init_levels()
@@ -20,7 +27,15 @@ approxSpace.init_surfaces()
 approxSpace.init_top_surface()
 approxSpace.print_statistic()
 
-dirichletBND = ug4.DirichletBoundary2d()
 
+# Create discretization.
+elemDisc = cd.ConvectionDiffusionFV12d("u", "INNER")
 
-domainDisc = ug4.DomainDiscretization2d(approxSpace)
+# dirichletBND = ug4.DirichletBoundary2d()
+
+domainDisc = ug4.DomainDiscretization2dCPU1(approxSpace)
+domainDisc.add(elemDisc)
+
+nstages = 2
+limex = limex.LimexTimeIntegrator2dCPU1(nstages)
+
